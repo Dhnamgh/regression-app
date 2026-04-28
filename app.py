618634236855b48203f6731f763bd0f544a7656f
Dhnamgh <danhchosv@gmail.com>
2026-04-28 14:06:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -790,6 +790,7 @@ def statsmodels_roc(y_true: np.ndarray, y_prob: np.ndarray):
 from sklearn.metrics import auc
 
 def auc_from_roc(fpr, tpr):
+    order = np.argsort(fpr)
     return float(auc(fpr[order], tpr[order]))
 def roc_outputs(model, y: pd.Series, X: pd.DataFrame):
     X_sm = sm.add_constant(X)
